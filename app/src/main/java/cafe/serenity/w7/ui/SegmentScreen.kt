70929d92149465bf7c92dc5008e1e290c272f184
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2024-11-17 02:41:35 +0400

    sector with rounded corners

--- app/src/main/java/cafe/serenity/w7/ui/SegmentScreen.kt
+++ app/src/main/java/cafe/serenity/w7/ui/SegmentScreen.kt
@@ -23,6 +23,9 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 @Composable
 fun Segment(modifier: Modifier, externalRadius: Dp, segmentWidth: Dp, startAngleDegrees: Float, sweepAngleDegrees: Float) {
@@ -32,26 +35,59 @@ fun Segment(modifier: Modifier, externalRadius: Dp, segmentWidth: Dp, startAngle
         val width = externalRadius * 2f
         val height = externalRadius * 2f
 
-        drawRect(Color.Blue, Offset(0f, 0f), Size((segmentWidth / 10f).toPx(), segmentWidth.toPx() / 10f))
+        val internalRadius = externalRadius - segmentWidth
 
-//        drawPath(Path().apply {
-//            arcTo(Rect(0f, 0f, width.toPx(), height.toPx()), startAngleDegrees, sweepAngleDegrees, false)
+        // Corners
+        val cornerRadius = segmentWidth.toPx() / 4f
+
+        val endAngleDegrees = startAngleDegrees + sweepAngleDegrees
+
+        val startRotationVector = PointF(cos(startAngleDegrees * 3.14f / 180f), sin(startAngleDegrees * 3.14f / 180f))
+        val endRotationVector = PointF(cos(endAngleDegrees * 3.14f / 180f), sin(endAngleDegrees * 3.14f / 180f))
+
+        drawPath(Path().apply {
+            arcTo(Rect(0f, 0f, width.toPx(), height.toPx()), startAngleDegrees, sweepAngleDegrees, false)
+
+            arcTo(Rect(
+                Offset((externalRadius.toPx() - cornerRadius + (externalRadius.toPx() - cornerRadius) * endRotationVector.x),
+                    externalRadius.toPx() - cornerRadius + (externalRadius.toPx() - cornerRadius) * endRotationVector.y),
+                Size(2f * cornerRadius, 2f * cornerRadius)), endAngleDegrees, 90f, false)
+
+            arcTo(Rect(
+                Offset(externalRadius.toPx() - cornerRadius + (internalRadius.toPx() + cornerRadius) * endRotationVector.x,
+                    externalRadius.toPx() - cornerRadius + (internalRadius.toPx() + cornerRadius) * endRotationVector.y),
+                Size(2f * cornerRadius, 2f * cornerRadius)), endAngleDegrees + 90f, 90f, false)
+
+            arcTo(
+                Rect(segmentWidth.toPx(),
+                    segmentWidth.toPx(),
+                    (2f * externalRadius - segmentWidth).toPx(),
+                    (2f * externalRadius - segmentWidth).toPx()),
+                startAngleDegrees + sweepAngleDegrees,
+                -sweepAngleDegrees,
+                false)
+
+            arcTo(Rect(
+                Offset(externalRadius.toPx() - cornerRadius + (internalRadius.toPx() + cornerRadius) * startRotationVector.x,
+                    externalRadius.toPx() - cornerRadius + (internalRadius.toPx() + cornerRadius) * startRotationVector.y),
+                Size(2f * cornerRadius, 2f * cornerRadius)), 180f + startAngleDegrees, 90f, false)
+
+            arcTo(Rect(
+                Offset((externalRadius.toPx() - cornerRadius + (externalRadius.toPx() - cornerRadius) * startRotationVector.x),
+                    externalRadius.toPx() - cornerRadius + (externalRadius.toPx() - cornerRadius) * startRotationVector.y),
+                Size(2f * cornerRadius, 2f * cornerRadius)), startAngleDegrees - 90f, 90f, false)
+
+        }, Color.Red)
+
+//        drawCircle(Color.Green,
+//            cornerRadius,
+//            Offset((externalRadius.toPx() + (externalRadius.toPx() - cornerRadius) * startRotationVector.x), externalRadius.toPx()  + (externalRadius.toPx() - cornerRadius) * startRotationVector.y))
 //
-////                        arcTo(Rect(0f, 0f, width.toPx(), height.toPx()), -90f, 90f, false)
-////                        arcTo(Rect(0f, 0f, width.toPx(), height.toPx()), 0f, 90f, false)
-//
-//            arcTo(
-//                Rect(segmentWidth.toPx(),
-//                    segmentWidth.toPx(),
-//                    (2f * externalRadius - segmentWidth).toPx(),
-//                    (2f * externalRadius - segmentWidth).toPx()),
-//                startAngleDegrees + sweepAngleDegrees,
-//                -sweepAngleDegrees,
-//                false)
-//        }, Color.Red)
-
-
+//        drawCircle(Color.Black,
+//            cornerRadius,
+//            Offset(externalRadius.toPx() + (internalRadius.toPx() + cornerRadius) * startRotationVector.x, externalRadius.toPx() + (internalRadius.toPx() + cornerRadius) * startRotationVector.y))
     }
+
 }
 
 @Composable
