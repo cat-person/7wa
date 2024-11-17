14c607b1d9078a54eb0c824a2ae057c865d13f93
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2024-11-18 02:33:34 +0400

    Put segment parameters into modifier

--- app/src/main/java/cafe/serenity/w7/ui/SegmentScreen.kt
+++ app/src/main/java/cafe/serenity/w7/ui/SegmentScreen.kt
@@ -12,15 +12,29 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.node.DrawModifierNode
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.node.ObserverModifierNode
+import androidx.compose.ui.node.invalidateDraw
+import androidx.compose.ui.node.observeReads
+import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
 import kotlin.math.PI
@@ -78,14 +92,6 @@ fun Segment(modifier: Modifier, externalRadius: Dp, segmentWidth: Dp, startAngle
                 Size(2f * cornerRadius, 2f * cornerRadius)), startAngleDegrees - 90f, 90f, false)
 
         }, Color.Red)
-
-//        drawCircle(Color.Green,
-//            cornerRadius,
-//            Offset((externalRadius.toPx() + (externalRadius.toPx() - cornerRadius) * startRotationVector.x), externalRadius.toPx()  + (externalRadius.toPx() - cornerRadius) * startRotationVector.y))
-//
-//        drawCircle(Color.Black,
-//            cornerRadius,
-//            Offset(externalRadius.toPx() + (internalRadius.toPx() + cornerRadius) * startRotationVector.x, externalRadius.toPx() + (internalRadius.toPx() + cornerRadius) * startRotationVector.y))
     }
 
 }
@@ -105,8 +111,7 @@ fun SegmentView(externalRadius: Dp, segmentWidth: Dp, startAngleDegrees: Float, 
         val height = externalRadius * 2f
         Segment(
             modifier = Modifier
-                .width(width)
-                .height(height)
+                .segmentDimens(externalRadius, segmentWidth, startAngleDegrees, sweepAngleDegrees)
                 .background(Color.Magenta)
 //                .graphicsLayer {
 //                    clip = true
@@ -130,5 +135,67 @@ fun SegmentView(externalRadius: Dp, segmentWidth: Dp, startAngleDegrees: Float, 
                     externalRadius, segmentWidth, startAngleDegrees, sweepAngleDegrees
         )
     }
+}
+
+private class SegmentDimensElement(private val externalRadius: Dp, private val segmentWidth: Dp, private val startAngleDegrees: Float, private val sweepAngleDegrees: Float): ModifierNodeElement<SegmentDimensNode>() {
+    override fun create(): SegmentDimensNode {
+        return SegmentDimensNode(externalRadius, segmentWidth, startAngleDegrees, sweepAngleDegrees)
+    }
+
+    override fun update(node: SegmentDimensNode) {
+        node.externalRadius = externalRadius
+        node.segmentWidth = segmentWidth
+        node.startAngleDegrees = startAngleDegrees
+        node.sweepAngleDegrees = sweepAngleDegrees
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SegmentDimensElement
+
+        if (externalRadius != other.externalRadius) return false
+        if (segmentWidth != other.segmentWidth) return false
+        if (startAngleDegrees != other.startAngleDegrees) return false
+        if (sweepAngleDegrees != other.sweepAngleDegrees) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = externalRadius.hashCode()
+        result = 31 * result + segmentWidth.hashCode()
+        result = 31 * result + startAngleDegrees.hashCode()
+        result = 31 * result + sweepAngleDegrees.hashCode()
+        return result
+    }
+}
+
+private class SegmentDimensNode(
+    var externalRadius: Dp,
+    var segmentWidth: Dp,
+    var startAngleDegrees: Float,
+    var sweepAngleDegrees: Float
+) : Modifier.Node(), ObserverModifierNode {
+
+    override fun onObservedReadsChanged() {
+
+    }
+}
+
+@Stable
+fun Modifier.segmentDimens(
+    externalRadius: Dp,
+    segmentWidth: Dp,
+    startAngleDegrees: Float,
+    sweepAngleDegrees: Float): Modifier {
+    return this.then(
+        SegmentDimensElement(
+            externalRadius,
+            segmentWidth,
+            startAngleDegrees,
+            sweepAngleDegrees)
+    )
 }
 
